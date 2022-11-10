ba8f31dbf44aafe90788eba08458ad06b91311be
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-11-10 14:26:52 +0000

    [Update]3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,6 +24,7 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+  
   def update
     list = List.find(params[:id])
     list.update(list_params)
