31cc778a8a9d9057cd6939a2826e1842b2c1a12a
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-11-11 14:16:38 +0000

    [Update]3-FlashMessage

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,13 @@ class ListsController < ApplicationController
   end
   
   def create
-    # １. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 2. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
-    flash[:notice] = "投稿が成功しました"
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice]="投稿が成功しました"
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
